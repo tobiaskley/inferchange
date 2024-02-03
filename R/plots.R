07cce3889c4e23dd6ed97ea6be31ebd4372ed1ac
housenli <housenli@ymail.com>
2024-02-03 18:22:00 +0100

    update plot for mcscan

--- R/plots.R
+++ R/plots.R
@@ -41,23 +41,24 @@ plot.inferchange.cp <- function(x, ...) {
   n = nrow(X)
   p = ncol(X)
   Xy =  X * matrix(y, nrow = n, ncol = p)
-  plt = ggplot(data.frame(sample_index = rep(1:n, p),
-                          covariance = as.vector(Xy)),
+  plt = ggplot(data.frame("sample_index" = rep(1:n, p),
+                          "covariance" = as.vector(Xy)),
                aes(x = sample_index, y = covariance)) +
     geom_line(color="gray")  +
     geom_vline(xintercept = x$cp, linetype = "dashed", color = "red") +
-    xlab("Index of sample") + ylab("Covariance of X and y (gray lines)") + theme_minimal() +
+    xlab("Index of sample") + ylab("Covariance of X and y (gray lines)") +
+    theme_minimal() +
     ggtitle("Estimated change points (red dashed vertical lines)")
   print(plt)
   if (!is.null(attr(x, "solution_path"))) {
     sopa = attr(x, "solution_path")
     id   = attr(x, "selected_solution")
-    plt  = ggplot(data.frame(value = unlist(sopa[,2]),
-                             ncp = sapply(sopa[,3], length)),
+    plt  = ggplot(data.frame("value" = unlist(sopa[,2]),
+                             "ncp" = sapply(sopa[,3], length)),
                   aes(x=ncp, y=value)) +
       geom_line() + geom_count(alpha = 0.5) +
-      geom_point(data = data.frame(ncp = length(unlist(sopa[id,"cps"])),
-                                   value = unlist(sopa[id,"val"])),
+      geom_point(data = data.frame("ncp" = length(unlist(sopa[id,"cps"])),
+                                   "value" = unlist(sopa[id,"val"])),
                  shape = 4, size = 3, color = "red")+
       ylab("Evidence of undetected change points") +
       xlab("Number of detected change points") +
